36a4350616d1952cf70c7f78335d0161b0a76832
Brae Troutman <troupb18@comp-21-3.deac.wfu.edu>
2022-10-24 21:59:34 -0400

    fix bad arithmetic syntax

--- run_scan2.sh
+++ run_scan2.sh
@@ -1,9 +1,9 @@
 make
-echo "num_threads,length,bc,parallel_scan_time" > data/scan2.csv
+echo "num_threads,length,bc,parallel_scan_time,scan_time" > data/scan2.csv
 for ((p=1; p<=22; p+=1))
 do
-	bc=$((1000000/$p))
+	bc=$((1000000/p))
 	./bin/scan2 10 1000000 $p $bc >> data/scan2.csv
-	echo executing with $p cores and a basecase of size $bc.
+	echo executing with $p cores and base case $bc
 done	
 
