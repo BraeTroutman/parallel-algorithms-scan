c7a553558eb771d40bacfdc0631723bfd8f51f4c
Brae Troutman <troupb18@comp-23-4.deac.wfu.edu>
2022-10-24 22:31:32 -0400

    update for 44 cores

--- run_scan1.sh
+++ run_scan1.sh
@@ -1,6 +1,6 @@
 make
 echo "num_threads,length,parallel_scan_time" > data/scan1.csv
-for ((p=1; p<=22; p+=1))
+for ((p=1; p<=44; p+=1))
 do
 	./bin/scan1 10 1000000 $p >> data/scan1.csv
 	echo executing with $p cores.
