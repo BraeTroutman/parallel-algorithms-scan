d0f1b512d6df5c0c5835fa8040f4e92489b200b9
Brae Troutman <troupb18@comp-23-3.deac.wfu.edu>
2022-10-25 10:42:53 -0400

    rerun with larger problem size to better demonstrate scaling

--- run_scan2.sh
+++ run_scan2.sh
@@ -3,7 +3,7 @@ echo "num_threads,length,bc,parallel_scan_time,scan_time" > data/scan2.csv
 for ((p=1; p<=44; p+=1))
 do
 	bc=$((1000000/p))
-	./bin/scan2 10 1000000 $p $bc >> data/scan2.csv
+	./bin/scan2 10 200000000 $p $bc >> data/scan2.csv
 	echo executing with $p cores and base case $bc
 done	
 
