329f005a62419bec29c49fb34a7b227f47a39dbf
Brae Troutman <troupb18@comp-23-4.deac.wfu.edu>
2022-10-24 22:26:16 -0400

    rerun everything with 44 avail cores

--- run_scan2.sh
+++ run_scan2.sh
@@ -1,6 +1,6 @@
 make
 echo "num_threads,length,bc,parallel_scan_time,scan_time" > data/scan2.csv
-for ((p=1; p<=22; p+=1))
+for ((p=1; p<=44; p+=1))
 do
 	bc=$((1000000/p))
 	./bin/scan2 10 1000000 $p $bc >> data/scan2.csv
