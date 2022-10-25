2ad81a7e76e452a8962d8f21aa6908285532ff00
Brae Troutman <troupb18@gemini.deac.wfu.edu>
2022-10-24 20:22:28 -0400

    script to produce scan1 parallel running time data

---
+++ run_scan1.sh
@@ -0,0 +1,7 @@
+echo "num_threads,length,parallel_scan_time,sequential_scan_time" > data/scan1.csv
+for ((p=1; p<=22; p+=1))
+do
+	./bin/scan1 10 1000000 $p >> data/scan1.csv
+	echo executing with $p cores.
+done	
+
